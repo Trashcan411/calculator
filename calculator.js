5b2e889da4e90406eed5ab00e48d0a45a197c06e
Trashcan411 <fahmiibrahim21193@gmail.com>
2025-03-23 13:08:15 +0700

    add html page

--- calculator.js
+++ calculator.js
@@ -1,4 +1,45 @@
-const arr = []
-const add = arr.reduce((accumulator, currentValue) => {
-    return accumulator + currentValue;}
-, arr[0])
+/*id = "equal" =
+id = "add" +
+id = "substract" -
+id = "clear-all" AC
+id = "backspace" ←
+id = "radical" √
+id = "power" ^
+id = "multiply" x 
+id = "divisive" /
+id = "minus" +/-
+id = "comma" .*/
+function add (...args){
+    const ad = args.reduce((accumulator, currentValue) => {
+        return accumulator + currentValue;}
+    , arr[0])
+    return ad
+    
+}
+function substract (...args){
+    const subs = args.reduce((accumulator, currentValue) => {
+        return accumulator - currentValue;}
+    , arr[0])
+    return subs
+}
+
+function multiply (...args){
+    const add = args.reduce((accumulator, currentValue) => {
+        return accumulator * currentValue;}
+    , arr[0])
+    
+}
+
+function multiply (...args){
+    const add = args.reduce((accumulator, currentValue) => {
+        return accumulator / currentValue;}
+    , arr[0])
+    
+}
+const nmbA = 0
+const nmbB = 0
+const variable = ""
+
+function operate (nmbA, nmbB, variable) {
+
+}
